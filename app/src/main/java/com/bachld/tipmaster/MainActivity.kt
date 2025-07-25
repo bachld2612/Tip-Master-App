217a48096aa8a6d3ab9b4af31f31fcea629cc7b6
bachld2612 <lyduybach900@gmail.com>
2025-07-25 19:42:58 +0700

    finish caculation logic

--- app/src/main/java/com/bachld/tipmaster/MainActivity.kt
+++ app/src/main/java/com/bachld/tipmaster/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.bachld.tipmaster
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var radioGroup: RadioGroup;
+    lateinit var input_hoaDon: EditText;
+    lateinit var input_tongTien: EditText;
+    lateinit var input_tienBoa: EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        radioGroup = findViewById(R.id.radioGroup);
+        input_hoaDon = findViewById(R.id.input_hoaDon);
+        input_tongTien = findViewById(R.id.input_tongTien);
+        input_tienBoa = findViewById(R.id.input_tienBoa);
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.radio_10 -> {
+                    val tien = input_hoaDon.text.toString().toDoubleOrNull() ?: 0.0
+                    val tienBoa = tien * 0.1
+                    val tongTien = tien + tienBoa
+                    input_tienBoa.setText(tienBoa.toString())
+                    input_tongTien.setText(tongTien.toString())
+                }
+                R.id.radio_15 -> {
+                    val tien = input_hoaDon.text.toString().toDoubleOrNull()?:0.0
+                    val tienBoa = tien * 0.15
+                    val tongTien = tien + tienBoa
+                    input_tienBoa.setText(tienBoa.toString())
+                    input_tongTien.setText(tongTien.toString())
+                }
+                R.id.radio_20 -> {
+                    val tien = input_hoaDon.text.toString().toDoubleOrNull() ?: 0.0
+                    val tienBoa = tien * 0.2
+                    val tongTien = tien + tienBoa
+                    input_tienBoa.setText(tienBoa.toString())
+                    input_tongTien.setText(tongTien.toString())
+                }
+            }
         }
     }
 }
